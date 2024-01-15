38eb8aebc240ad49cc98fb27e7dcd71c828f4143
pedrogilsenarego <pedrogilsenarego@gmail.com>
2024-01-15 21:26:47 +0000

    a

--- src/pages/Complex/index.tsx
+++ src/pages/Complex/index.tsx
@@ -1,4 +1,10 @@
-import { Container, Grid, Typography } from "@mui/material";
+import {
+  Container,
+  Grid,
+  Typography,
+  useMediaQuery,
+  useTheme,
+} from "@mui/material";
 
 import Carousel from "../../components/Carousel";
 import Button from "../../components/Ui/Button";
@@ -6,6 +12,8 @@ import SchedulleMembership from "./Components/SchedulleMembership";
 import Team from "./Components/Team";
 
 const Complex = () => {
+  const theme = useTheme();
+  const mobile = useMediaQuery(theme.breakpoints.down("sm"));
   const childrenData = [
     {
       link: "https://ef-medispa.imgix.net/storage/uploads/homepage/efmedispa-homepage-header-image_vgtvo.jpg?w=1300&q=95&auto=format&fit=crop&crop=edges,focalpoint&fm=png",
@@ -23,11 +31,11 @@ const Complex = () => {
 
   return (
     <>
-      <div style={{ marginTop: "230px" }}>
+      <div style={{ marginTop: mobile ? "100px" : "230px" }}>
         <Carousel
           navButton
           width={"100%"}
-          height={`calc(100vh - 330px)`}
+          height={mobile ? "600px" : `calc(100vh - 330px)`}
           pauseDuration={3000}
           slideDuration={1000}
           direction={1}
@@ -39,17 +47,54 @@ const Complex = () => {
           onItemClick={(item) => console.log(item)}
         >
           {childrenData.map((item, index) => (
-            <img
+            <div
               draggable={false}
-              alt=""
               key={item.link}
-              src={item.link}
               style={{
-                height: `calc(100vh - 330px)`,
+                backgroundImage: `url(${item.link})`,
+                backgroundSize: "cover",
+                backgroundPosition: "center center",
+                height: mobile ? "600px" : `calc(100vh - 330px)`,
                 objectFit: "cover",
                 width: "100%",
+                padding: "40px",
               }}
-            />
+            >
+              <div
+                style={{
+                  display: "flex",
+                  flexDirection: "column",
+                  justifyContent: "end",
+                  alignItems: "end",
+                  height: "100%",
+                  rowGap: "10px",
+                  paddingBottom: mobile ? "30px" : undefined,
+                }}
+              >
+                <Typography
+                  style={{
+                    fontSize: mobile ? "14px" : "25px",
+                    textTransform: "uppercase",
+                    fontWeight: "bold",
+                    width: mobile ? "100%" : "30%",
+                    textAlign: "center",
+                  }}
+                >
+                  Título 1-4
+                </Typography>
+                <Typography
+                  style={{
+                    fontSize: mobile ? "12px" : "25px",
+                    textTransform: "uppercase",
+                    width: mobile ? "100%" : "30%",
+                    textAlign: "center",
+                  }}
+                >
+                  Descrição Descrição Descrição Descrição Descrição Descrição
+                  Descrição Descrição
+                </Typography>
+              </div>
+            </div>
           ))}
         </Carousel>
       </div>
